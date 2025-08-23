216bb716ff0ff4ff9ebf3694c3da9fdce70c771f
James Fishwick <fishwick@gmail.com>
2025-08-22 20:09:20 -0400

    Remove test file

--- test_precommit.py
+++
@@ -1,5 +0,0 @@
-# Intentionally bad code to test pre-commit hooks
-import sys,os # Bad import formatting
-def   bad_function(  ):  # Bad spacing
-    print("This has trailing whitespace") 
-    return "test"   
